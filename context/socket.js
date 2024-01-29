62c9948097deda2a651e5e633f7f8aca831cf395
Anurag <anuragnimkar21@gmail.com>
2024-01-29 10:31:37 +0530

    first phase

--- context/socket.js
+++ context/socket.js
@@ -1,26 +1,29 @@
+import { createContext, useContext, useEffect, useState } from "react";
 import { io } from "socket.io-client";
-import { useState, useEffect, Children , createContext, useContext} from "react";
 
-const  SocketContext = createContext(null)
+const SocketContext = createContext(null);
 
-export const useSocket =()=>{
+export const useSocket = () => {
     const socket = useContext(SocketContext)
     return socket
 }
-export  const SocketProvider = (props)=>{
 
-    const {children} = props;
-    const [socket, setSocket] = useState(null)
-    
+export const SocketProvider = (props) => {
+  const { children } = props;
+  const [socket, setSocket] = useState(null);
 
-    useEffect(()=>{
-        const connection = io()
-        setSocket(connection)
-    }, [])
+  useEffect(() => {
+    const connection = io();
+    console.log("socket connection", connection)
+    setSocket(connection);
+  }, []);
 
-    socket?.on('connect_error' , async (err) =>{
-        console.log("error estavlishing socket" , err)
-        await fetch(' ./api/socket')
-    })
-    return (<SocketContext.Provider value = {socket}>{children}</SocketContext.Provider>);
-}
+  socket?.on('connect_error', async (err) => {
+    console.log("Error establishing socket", err)
+    await fetch('/api/socket')
+  })
+
+  return (
+    <SocketContext.Provider value={socket}>{children}</SocketContext.Provider>
+  );
+};
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import '@/styles/globals.css'
 import { SocketProvider } from '@/context/socket'
+
 export default function App({ Component, pageProps }) {
   return (
 
